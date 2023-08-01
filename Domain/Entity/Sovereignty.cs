db99bee9181f78cd549dd9d969f6107cba9d1ca4
StevenJBurns <StevenJBurns@users.noreply.github.com>
2023-08-01 15:49:17 -0600

    feat: create Sovereignty enum

---
+++ Domain/Entity/Sovereignty.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Ara.PFT.Model.Map;
+
+public enum Sovereignty
+{
+  NotSet = -1,
+  Level0 = 0,
+  Level1 = 1,
+  Level2 = 2,
+  Level3 = 3,
+  Level4 = 4,
+  Level5 = 5
+}
