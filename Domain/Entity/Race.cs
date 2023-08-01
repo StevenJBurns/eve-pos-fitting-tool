b1e4cc3e2cf9192367962d4247df085e45fc2dbd
StevenJBurns <StevenJBurns@users.noreply.github.com>
2023-08-01 16:46:59 -0600

    feat: create Race enum

---
+++ Domain/Entity/Race.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ara.PFT.Model.POS;
+
+public enum Race
+{
+  NotSet = 0,
+  Amarr = 1,
+  Caldari = 2,
+  Gallente = 3,
+  Minmitar = 4
+}
